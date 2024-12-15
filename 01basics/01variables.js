f80f77ef9f60e4bfcac08d434a2d462e0d9ab8f1
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2024-12-15 16:37:06 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,14 @@
+const accountID=144553
+let accountemail="gajanan@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+let accountstate;
+// accountid 2 is not allowed
+console.log(accountID);
+
+accountpassword="211221"
+accountcity="pune"
+accountemail="gpgp@gmail"
+
+// prefer not to use var beacuse of issue in block and fucntionalk sope 
+console.table([accountID,accountcity,accountpassword,accountemail,accountstate]);
---
+++ 01basics/datatype.js
@@ -0,0 +1,14 @@
+//"use strict";  //treat all js code as newer version
+
+// alert(3+3)  we are using node js not browser
+let name="gajanan" //string
+let age=18 //int  
+//boolean=> true/false
+//null  > standalone value
+//undefined 
+//symbol => uniqueness (figma react)
+
+//object
+
+console.log(typeof age );
+console.log(typeof null); //null is object and undefined is undefined
