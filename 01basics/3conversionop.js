7cc4389cbe9e2297ce1b863c67e7a63176e49e35
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2024-12-15 16:52:46 +0000

    conversion of datatypes and operations

---
+++ 01basics/3conversionop.js
@@ -0,0 +1,18 @@
+let score="33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valinnum=Number(score)
+// console.log(typeof valinnum);
+// console.log( valinnum); // for null and undefiedn it nan
+//33 abc => Nan type which is also a num
+
+let logined=1;//1>= true   0=false
+let letlogin= Boolean(logined)
+// console.log(letlogin);
+
+let somenum=23;
+let somestring=String(somenum)
+console.log(somenum)
+console.log(typeof somestring);
+
