6f9e40ec1afffb6d0e44c7edb5535f9d93304208
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2024-12-22 16:39:10 +0000

    functions

--- 01basics/3conversionop.js
+++ 01basics/3conversionop.js
@@ -2,12 +2,12 @@
 
 let score="33"
 
-// console.log(typeof score);
-// console.log(typeof(score));
-// let valinnum=Number(score)
-// console.log(typeof valinnum);
-// console.log( valinnum); // for null and undefiedn it nan
-//33 abc => Nan type which is also a num
+console.log(typeof score);
+console.log(typeof(score));
+let valinnum=Number(score)
+console.log(typeof valinnum);
+console.log( valinnum); // for null and undefiedn it nan
+// 33 abc => Nan type which is also a num
 
 let logined=1;//1>= true   0=false
 let letlogin= Boolean(logined)
@@ -15,14 +15,14 @@ let letlogin= Boolean(logined)
 
 let somenum=23;
 let somestring=String(somenum)
-// console.log(somenum)
-// console.log(typeof somestring);
+console.log(somenum)
+console.log(typeof somestring);
 
 
-// console.log("2">1)
-// console.log(null>=0);   //true     // null is converted to zero number by javascript
-// console.log(null>0);        //false
-// console.log(null==0);       //false
+console.log("2">1)
+console.log(null>=0);   //true     // null is converted to zero number by javascript
+console.log(null>0);        //false
+console.log(null==0);       //false
 
 // console.log(undefined==0);      //all cases >= <= == gives as false
 
---
+++ 03basics/01function.js
@@ -0,0 +1,91 @@
+function addtwo(num1 ,num2)
+{
+    console.log( num1+num2);
+    }
+    
+// addtwo(2,3)   //5
+
+// addtwo(2,"3")   //23
+// addtwo("2",3) //23
+// addtwo(3,null)   ///3
+// const result = addtwo(3,4) 
+// console.log(result);   // undefined not 7
+
+// function returnsum(n1,n2){
+//   let   result=n1+n2
+//     return result
+//     // nothing is printed after return statement
+// }
+
+// console.log( returnsum(4,3));   //7 
+
+//  function loginmessage(username){
+//     if(username===undefined /* or !username */){
+//         console.log("enter a username")
+//         return null
+//     }
+// return `${username} just logged in`
+
+//  }
+
+//  console.log(loginmessage());  // undefined if no input
+ 
+
+// function calculatecartprice(val1,val2,...num1){
+//     return num1
+// }
+// console.log(calculatecartprice(200,400,500));  //[ 200, 400, 500 ] converts to array by  ...
+
+ 
+// function calculatecartprice(val1,val2,...num1){
+//     return num1
+// }
+// console.log(calculatecartprice(200,400,500)); //val1 ==200  val2=400 remaining in  num1 => [500]
+
+const user={
+    username:"gajanan",
+    price:199
+}
+
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);   
+}
+// handleobject(user)  //username is gajanan and price is 199
+
+// direct object passing in function & arrays also 
+
+handleobject({
+username:"arya",
+price:80000
+})   //username is arya and price is 80000
+
+
+
+const myarray=[20,52,715,64]
+function returnarray(getarray)
+{
+    
+return getarray[1]
+}
+// console.log(returnarray(myarray));  //52 first index element
+
+console.log(returnarray([1,9,34,5,6,7]));  //9 first index element 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
