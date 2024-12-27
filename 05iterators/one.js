bcb6295c9a2a594345cbdeb2954af74d4a2dc25c
gajanan patange <123582811+gajananhp13@users.noreply.github.com>
2024-12-27 18:55:47 +0000

    for,While,do-Whileloops

--- 05iterators/one.js
+++ 05iterators/one.js
@@ -1,9 +1,36 @@
 // node 05iterators/one.js
 // ctrl d and it helps to select multiple items of same name 
-const array=[1,23,432,51,19]
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
- console.log(element
- );
+// const array=[1,23,432,51,19]
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//  console.log(element
+//  );
     
+// }
+
+
+// while and do while ______________________________
+
+let index=0;
+while (index<=5) {
+
+   console.log(index);
+ index++
+   // index=index+2
+}
+
+let hero=["superman","flash","spiderman",null]
+let arr=0
+while (arr<hero.length) {
+    console.log(`the arr is ${hero[arr]}`);
+    
+    arr++
+}
+let score=1
+do {
+    console.log(`score is ${score}`);
+     score++
+}
+while (score<5) {
+   
 }
